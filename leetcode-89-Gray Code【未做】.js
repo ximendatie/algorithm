e185b82bde870192a5bb6d60cd6bf40916cdb253
harvey <heweimr@gmail.com>
2017-03-23 23:24:50 +0800

    89

--- leetcode-89-Gray Code【未做】.js
+++ leetcode-89-Gray Code【未做】.js
@@ -8,6 +8,12 @@
  */
 
 
-var grayCode = function(n) {
+// var grayCode = function(n) {
     
-};
+// };
+// 别人的java
+public List<Integer> grayCode(int n) {
+    List<Integer> result = new LinkedList<>();
+    for (int i = 0; i < 1<<n; i++) result.add(i ^ i>>1);
+    return result;
+}
